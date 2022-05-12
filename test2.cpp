2e0c2d40f701b98a47cd517fc90efa62cfdb979c
Divyanenavath98 <105106849+Divyanenavath98@users.noreply.github.com>
2022-05-12 11:22:44 -0700

    Codechange

---
+++ test2.cpp
@@ -0,0 +1,35 @@
+#define CATCH_CONFIG_MAIN  
+#include "catch.hpp"
+#include "stats.h"
+#include <cmath>
+TEST_CASE("reports average, minimum and maximum") {
+	Statistics::Stat<float> computedvalues;
+    Statistics::Stat<float> computedstats = Statistics::ComputeStatistics(computedvalues,{1.5, 8.9, 3.1, 4.5});
+    float epsilon = 0.001;
+    REQUIRE(std::abs(computedstats.average - 4.525) < epsilon);
+    REQUIRE(std::abs(computedstats.Max - 8.9) < epsilon);
+    REQUIRE(std::abs(computedstats.Min - 1.5) < epsilon);
+}
+TEST_CASE("average is NaN for empty array") {
+	 Statistics::Stat<float> computedvalues;
+    Statistics::Stat<float> computedstats = Statistics::ComputeStatistics(computedvalues, {});
+    REQUIRE(isnan(computedstats.average));
+    REQUIRE(isnan(computedstats.Max));
+    REQUIRE(isnan(computedstats.Min));
+}
+TEST_CASE("reports average, minimum and maximum") {
+	std::vector<float> data{1.5, 8.9, 3.1, 4.5};
+    auto computedStats=Statistics::ComputeStatistics(data);
+    float epsilon = 0.001;
+    REQUIRE(std::abs(computedStats.Average - 4.525) < epsilon);
+    REQUIRE(std::abs(computedStats.Max - 8.9) < epsilon);
+    REQUIRE(std::abs(computedStats.Min - 1.5) < epsilon);
+}
+
+TEST_CASE("average is NaN for empty array") {
+	std::vector<float> data{};
+    auto computedStats = Statistics::ComputeStatistics(data);
+    REQUIRE(isnan(computedStats.Average));
+    REQUIRE(isnan(computedStats.Max));
+    REQUIRE(isnan(computedStats.Min));
+}
